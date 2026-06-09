92b45379161ce22e26989ff7f565f7aa0439d933
monaliagarwal <monaliagarwal22@gmail.com>
2026-06-09 14:03:46 +0530

    fix CSRF

--- OneDrive/Desktop/college complain/collegevoice/settings.py
+++ OneDrive/Desktop/college complain/collegevoice/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c8eup!z!y_q2=xa^+x5e%
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://college-complaint-site-production.up.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
